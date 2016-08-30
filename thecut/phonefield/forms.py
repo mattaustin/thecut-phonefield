4d5dd96498051dfd8c16665553e75185551fecd5
Matt Austin <matt.austin@thecut.net.au>
2016-08-30 11:09:09 +0800

    Provide a more generic 'international' base ``PhoneNumberField`` and ``PhoneNumberValidator``.

--- thecut/phonefield/forms.py
+++ thecut/phonefield/forms.py
@@ -7,23 +7,41 @@ import phonenumbers
 import re
 
 
-class AUPhoneNumberField(CharField):
+class PhoneNumberField(CharField):
 
-    default_validators = [validators.validate_auphonenumber]
+    default_validators = []
 
     phone_number_format = phonenumbers.PhoneNumberFormat.INTERNATIONAL
 
-    phone_number_placeholder = '(xx) xxxx xxxx'
+    phone_number_placeholder = '+xx...'
 
-    phone_number_region = 'AU'
+    phone_number_region = None
 
     widget = widgets.PhoneInput
 
+    def __init__(self, *args, **kwargs):
+        phone_number_format = kwargs.pop('format', None)
+        phone_number_placeholder = kwargs.pop('region', None)
+        phone_number_region = kwargs.pop('region', None)
+
+        if phone_number_format is not None:
+            self.phone_number_format = phone_number_format
+        if phone_number_placeholder is not None:
+            self.phone_number_placeholder = phone_number_placeholder
+        if phone_number_region is not None:
+            self.phone_number_region = phone_number_region
+
+        if not self.default_validators:
+            self.default_validators = [
+                validators.PhoneNumberValidator(
+                    region=self.phone_number_region)]
+
+        super(PhoneNumberField, self).__init__(*args, **kwargs)
+
     def clean(self, *args, **kwargs):
-        value = super(AUPhoneNumberField, self).clean(*args, **kwargs)
+        value = super(PhoneNumberField, self).clean(*args, **kwargs)
         if value in EMPTY_VALUES:
             return ''
-
         value = phonenumbers.parse(value, self.phone_number_region)
         value = phonenumbers.format_number(value, self.phone_number_format)
         return re.sub(r'[^+\d+]+', '', value)
@@ -32,7 +50,20 @@ class AUPhoneNumberField(CharField):
         return utils.format_for_display(value, self.phone_number_region)
 
     def widget_attrs(self, *args, **kwargs):
-        widget_attrs = super(AUPhoneNumberField, self).widget_attrs(*args,
-                                                                    **kwargs)
+        widget_attrs = super(PhoneNumberField, self).widget_attrs(*args,
+                                                                  **kwargs)
         widget_attrs.setdefault('placeholder', self.phone_number_placeholder)
         return widget_attrs
+
+
+class AUPhoneNumberField(PhoneNumberField):
+
+    default_validators = [
+        validators.PhoneNumberValidator(
+            region='AU',
+            message='Enter a valid Australian phone number. Please ensure the '
+                    'full phone number is provided.')]
+
+    phone_number_placeholder = '(xx) xxxx xxxx'
+
+    phone_number_region = 'AU'
