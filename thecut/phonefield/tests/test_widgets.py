15397343274affb422aac32a5e6621a03b0bda45
Matt Austin <matt.austin@thecut.net.au>
2016-08-30 11:23:14 +0800

    Remove stray and unused import from test_widgets.

--- thecut/phonefield/tests/test_widgets.py
+++ thecut/phonefield/tests/test_widgets.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from ..widgets import PhoneInput
-from django.core import mail
 from django.test import TestCase
 
 
