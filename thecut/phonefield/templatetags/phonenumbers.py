4007d64922ba371ca675807fbe9895196cc98dee
Jayden Spencer <jayden.spencer@thecut.net.au>
2017-07-05 12:11:46 +0800

    phone_number templatetag with optional paramater for region  and uri_phone_number for links - Refs #11310

--- thecut/phonefield/templatetags/phonenumbers.py
+++ thecut/phonefield/templatetags/phonenumbers.py
@@ -7,6 +7,17 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name='phone_number')
+def phone_number(value, region=None):
+    return utils.format_for_display(value, region)
+
+
+@register.filter(name='uri_phone_number')
+def uri_phone_number(value):
+    return utils.format_for_uri(value)
+
+
+# Deprecated - please use phone_number
 @register.filter(name='au_phone_number')
 def au_phone_number(value):
     return utils.format_for_display(value, region='AU')
